c0ae22a4a55b6cee846eec5b0eb085166a9190db
oshkoshbagoshh <amirjavadi25@gmail.com>
2024-03-11 17:36:48 -0500

    updated backUpDownloads script

--- backupDownloads.js
+++ backupDownloads.js
@@ -4,8 +4,38 @@ const fse = require('fs-extra');
 const path = require('path');
 require('dotenv').config();
 
-// Now you can use process.env to access your variables
+// Accessing environment variables
 console.log(process.env.downloadsFolderPath);
-// define the paths according to your system
-// print all environment variables
-console.table(process.env);
+console.log(process.env.externalDrivePath);
+
+const downloadsFolderPath = process.env.downloadsFolderPath;
+const externalDrivePath = process.env.externalDrivePath;
+const zipFileName = 'DownloadsBackup.zip';
+const outputZipPath = path.join(externalDrivePath, zipFileName);
+
+// Create a file to stream archive data to.
+const output = fs.createWriteStream(outputZipPath);
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+
+// Listen for errors
+archive.on('error', function(err) {
+  throw err;
+});
+
+// Pipe archive data to the output file
+archive.pipe(output);
+
+// Append files from the Downloads folder and finalize the archive
+archive.directory(downloadsFolderPath, false);
+archive.finalize();
+
+// Listen for completion of the archive operation
+output.on('close', function() {
+  console.log(`Backup has been finalized. Total size: ${archive.pointer()} bytes`);
+  // Since we're already writing directly to the external drive, no need to move the file again.
+  console.log('File has been saved successfully');
+});
+
+// Optional: If you need to move the file after creation for some reason, use fse.move here.
